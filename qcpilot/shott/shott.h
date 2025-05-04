4e67e63eae75207d00be071449fe06a22052d6ff
HE Qichen <heqichen@gmail.com>
2025-05-05 01:54:49 +0800

    Can send engine rpm to multicast

--- qcpilot/shott/shott.h
+++ qcpilot/shott/shott.h
@@ -1,9 +1,12 @@
 #ifndef __QCPILOT_SHOTT_SHOTT_H__
 #define __QCPILOT_SHOTT_SHOTT_H__
 
+#include <arpa/inet.h>
+#include <net/if.h>
 #include <memory>
 #include "cereal/messaging/messaging.h"
 #include "openpilot/common/ratekeeper.h"
+#include "openpilot/qcpilot/shott/data_def.h"
 
 namespace qcpilot {
 namespace shott {
@@ -17,7 +20,11 @@ class Shott {
     RateKeeper rateKeeper_;
     bool isMyselfNotLagging_ {false};
     SubMaster sm_;
-    std::unique_ptr<SubMaster> subMasterPtr_;
+
+    struct ConsoleFrame frame_;
+
+    int socketFd_ {-1};
+    struct sockaddr_in mcastAddr_ {};
 
     void step();
 };
