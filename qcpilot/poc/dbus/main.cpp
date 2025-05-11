77c5d44fbaf5b7a9786d9ebd9e6ebbf4b98df7aa
HE Qichen <heqichen@gmail.com>
2025-05-11 21:26:10 +0800

    try to using qt

--- qcpilot/poc/dbus/main.cpp
+++ qcpilot/poc/dbus/main.cpp
@@ -1,10 +1,28 @@
 #include <QCoreApplication>
+#include <QDBusInterface>
 #include <QDebug>
 
-int main(int argc, char *argv[]) {
-  QCoreApplication app(argc, argv);
+int main(int argc, char **argv) {
+    QCoreApplication app(argc, argv);
 
-  qDebug() << "Hello, Qt console application!";
+    if (!QDBusConnection::systemBus().isConnected()) {
+        qWarning().noquote() << "complexping"
+                             << "Cannot connect to the D-Bus session bus.\n"
+                                "To start it, run:\n"
+                                "\teval `dbus-launch --auto-syntax`";
+        return 1;
+    } else {
+        qDebug() << "success";
+    }
 
-  return QCoreApplication::exec();
+    // QDBusServiceWatcher serviceWatcher(
+    //   SERVICE_NAME, QDBusConnection::systemBus(), QDBusServiceWatcher::WatchForRegistration);
+
+    // Ping ping;
+    // QObject::connect(&serviceWatcher, &QDBusServiceWatcher::serviceRegistered, &ping, &Ping::start);
+
+    // QProcess pong;
+    // pong.start("./complexpong");
+
+    app.exec();
 }
