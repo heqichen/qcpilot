6f171a6899024e22e6beed20af9992411678377d
HE Qichen <heqichen@gmail.com>
2025-05-14 12:44:57 +0800

    Can compile

--- qcpilot/shott/main.cpp
+++ qcpilot/shott/main.cpp
@@ -1,14 +1,19 @@
 #include <QCoreApplication>
 #include "network.h"
+#include "openpilot/common/ratekeeper.h"
 
 int main(int argc, char *argv[], const char *envs[]) {
     std::ignore = envs;
+    RateKeeper rateKeeper {"shott", 1};
     QCoreApplication app(argc, argv);
     qcpilot::Network network {};
-
+    bool isMyselfNotLagging {false};
     while (true) {
+        isMyselfNotLagging = !rateKeeper.keepTime();
+        std::ignore = isMyselfNotLagging;
         app.processEvents();
         network.step();
+        printf("%d\r\n", isMyselfNotLagging);
         if (network.isDirty()) {
             printf("network changed\r\n");
             network.clear();
