8dc74e6931c62c4e5873d468da1855aa2ac96ddb
HE Qichen <heqichen@gmail.com>
2025-05-15 22:44:06 +0800

    Can send speed

--- qcpilot/shott/main.cpp
+++ qcpilot/shott/main.cpp
@@ -1,29 +1,26 @@
-// #include <QCoreApplication>
-// #include "network.h"
+#include <cassert>
 #include "openpilot/common/ratekeeper.h"
 #include "openpilot/qcpilot/shott/shott.h"
+#include "system/hardware/hw.h"
 
 int main(int argc, char *argv[], const char *envs[]) {
     std::ignore = argc;
     std::ignore = argv;
     std::ignore = envs;
+    if (!Hardware::PC()) {
+        int ret;
+        ret = util::set_realtime_priority(53);
+        assert(ret == 0);
+        ret = util::set_core_affinity({4});
+        assert(ret == 0);
+    }
     RateKeeper rateKeeper {"shott", 50};
-    // QCoreApplication app(argc, argv);
-    // qcpilot::Network network {};
     qcpilot::Shott shott {};
     bool isMyselfNotLagging {false};
     while (true) {
         isMyselfNotLagging = !rateKeeper.keepTime();
         std::ignore = isMyselfNotLagging;
-        // app.processEvents();
-        // network.step();
-        // if (network.isDirty()) {
-        //     network.clear();
-        //     shott.rebindNetwork();
-        // }
         shott.step();
-
-        // printf("%d\r\n", isMyselfNotLagging);
     }
     return 0;
 }
