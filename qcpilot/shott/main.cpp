42ad6d9310e6ddf55c3255983cf3b363f9bacea3
HE Qichen <heqichen@gmail.com>
2025-05-15 20:43:56 +0800

    Remove qt dependency

--- qcpilot/shott/main.cpp
+++ qcpilot/shott/main.cpp
@@ -1,24 +1,26 @@
-#include <QCoreApplication>
-#include "network.h"
+// #include <QCoreApplication>
+// #include "network.h"
 #include "openpilot/common/ratekeeper.h"
 #include "openpilot/qcpilot/shott/shott.h"
 
 int main(int argc, char *argv[], const char *envs[]) {
+    std::ignore = argc;
+    std::ignore = argv;
     std::ignore = envs;
     RateKeeper rateKeeper {"shott", 50};
-    QCoreApplication app(argc, argv);
-    qcpilot::Network network {};
+    // QCoreApplication app(argc, argv);
+    // qcpilot::Network network {};
     qcpilot::Shott shott {};
     bool isMyselfNotLagging {false};
     while (true) {
         isMyselfNotLagging = !rateKeeper.keepTime();
         std::ignore = isMyselfNotLagging;
-        app.processEvents();
-        network.step();
-        if (network.isDirty()) {
-            network.clear();
-            shott.rebindNetwork();
-        }
+        // app.processEvents();
+        // network.step();
+        // if (network.isDirty()) {
+        //     network.clear();
+        //     shott.rebindNetwork();
+        // }
         shott.step();
 
         // printf("%d\r\n", isMyselfNotLagging);
